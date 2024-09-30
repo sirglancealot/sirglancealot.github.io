177042b3b9cfc39ff6c7310f562af23cf05d31a2
sirglancealot <jkslot@gmail.com>
2024-09-30 22:13:38 +0200

    test

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ function showTime() {
         h = 12;
     }
 
-    if (h > 12) {
-        h = h - 12;
-        session = "PM";
-    }
+    // if (h > 12) {
+    //     h = h - 12;
+    //     session = "PM";
+    // }
 
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
