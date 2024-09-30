369fec6421fdb4b5f791f2b03c4c6cedf47a040d
sirglancealot <jkslot@gmail.com>
2024-09-30 22:16:27 +0200

    test

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ function showTime() {
         h = 12;
     }
 
-    // if (h > 12) {
-    //     h = h - 12;
-    //     session = "PM";
-    // }
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
 
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
 
-    var time = h + ":" + m + ":" + s + " " + session;
+    var time = h + ":" + m;// + ":" + s + " " + session;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
 
