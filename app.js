0ca1ddfd64aae4a37f7bcf22a6684629be352f25
sirglancealot <jkslot@gmail.com>
2024-10-08 12:21:30 +0200

    test

--- app.js
+++ app.js
@@ -29,26 +29,28 @@ function showTime() {
 
   setTimeout(showTime, 1000);
 }
-
+forecastData();
 function forecastData() {
   var BrowserLatitude;
   var BrowserLongitude;
-  getLocation();//.then(console.log(BrowserLatitude));
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        // Assign new values of latitude and longitude inside the function
-        BrowserLatitude = position.coords.latitude;
-        BrowserLongitude = position.coords.longitude;
-      });
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
+ 
+  var getPosition = function (options) {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
   }
-
+  
+  getPosition()
+  .then((position) => {
+    console.log(position);
+    BrowserLatitude = position.latitude;
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
   
   //var WeatherEndpoint = "https://api.open-meteo.com/v1/forecast?latitude=" + BrowserLatitude + "&longitude=" + BrowserLongitude + "&minutely_15=temperature_2m,relative_humidity_2m,apparent_temperature,rain,snowfall,weather_code,wind_speed_10m,lightning_potential,is_day&hourly=temperature_2m,rain,cloud_cover,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&forecast_days=3&models=dmi_seamless";
-  //console.log(WeatherEndpoint);
+  console.log(BrowserLatitude);
   var NewEndpoint =
     "https://api.open-meteo.com/v1/forecast?latitude=56.567&longitude=9.0271&minutely_15=temperature_2m,relative_humidity_2m,apparent_temperature,rain,snowfall,weather_code,wind_speed_10m,lightning_potential,is_day&hourly=temperature_2m,rain,cloud_cover,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&forecast_days=3&models=dmi_seamless";
   fetch(NewEndpoint)
