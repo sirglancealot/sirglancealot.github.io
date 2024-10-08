39d0c7b0c0429bf0deadc5a40e3401df7cc8b6f0
sirglancealot <jkslot@gmail.com>
2024-10-08 11:05:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function forecastData() {
   //var BrowserLongitude = BrowserPosition.longitude;
 
   //var WeatherEndpoint = "https://api.open-meteo.com/v1/forecast?latitude=" + BrowserLatitude + "&longitude=" + BrowserLongitude + "&minutely_15=temperature_2m,relative_humidity_2m,apparent_temperature,rain,snowfall,weather_code,wind_speed_10m,lightning_potential,is_day&hourly=temperature_2m,rain,cloud_cover,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&forecast_days=3&models=dmi_seamless";
-  console.log(WeatherEndpoint);
+  //console.log(WeatherEndpoint);
   var NewEndpoint =
     "https://api.open-meteo.com/v1/forecast?latitude=56.567&longitude=9.0271&minutely_15=temperature_2m,relative_humidity_2m,apparent_temperature,rain,snowfall,weather_code,wind_speed_10m,lightning_potential,is_day&hourly=temperature_2m,rain,cloud_cover,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&forecast_days=3&models=dmi_seamless";
   fetch(NewEndpoint)
