d44e99a9936cd2826dda4200071e28c1cda657a9
sirglancealot <jkslot@gmail.com>
2024-10-12 00:12:58 +0200

    Removing console logging

--- app.js
+++ app.js
@@ -42,13 +42,13 @@ function GetCurrentData() {
    
   getPosition()
   .then((position) => {
-    console.log(position);
+    //console.log(position);
     BrowserLatitude = position.coords.latitude;
     BrowserLongitude = position.coords.longitude;
   
     var WeatherEndpoint = "https://api.open-meteo.com/v1/forecast?latitude=" + BrowserLatitude + "&longitude=" + BrowserLongitude + "&minutely_15=temperature_2m,relative_humidity_2m,apparent_temperature,rain,snowfall,weather_code,wind_speed_10m,lightning_potential,is_day&timezone=Europe%2FBerlin&forecast_days=3&models=dmi_seamless";
     //var WeatherEndpoint = "https://api.open-meteo.com/v1/forecast?latitude=" + BrowserLatitude + "&longitude=" + BrowserLongitude + "&minutely_15=temperature_2m,relative_humidity_2m,apparent_temperature,rain,snowfall,weather_code,wind_speed_10m,lightning_potential,is_day&hourly=temperature_2m,rain,cloud_cover,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&forecast_days=3&models=dmi_seamless";
-    console.log(WeatherEndpoint);
+    //console.log(WeatherEndpoint);
     fetch(WeatherEndpoint)
       .then((response) => response.json())
       .then((data) => weatherData(data))
@@ -130,12 +130,12 @@ function GetDailyForecastData() {
 
   getPosition()
   .then((position) => {
-    console.log(position);
+    //console.log(position);
     BrowserLatitude = position.coords.latitude;
     BrowserLongitude = position.coords.longitude;
   
     var WeatherEndpoint = "https://api.open-meteo.com/v1/forecast?latitude=" + BrowserLatitude + "&longitude=" + BrowserLongitude + "&daily=weather_code,temperature_2m_max,temperature_2m_min,daylight_duration,sunshine_duration,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant&timezone=Europe%2FBerlin&forecast_days=3&models=dmi_seamless";
-    console.log(WeatherEndpoint);
+    //console.log(WeatherEndpoint);
     
     // Endpoint fixed to Skive
     // https://api.open-meteo.com/v1/forecast?latitude=56.567&longitude=9.0271&daily=weather_code,temperature_2m_max,temperature_2m_min,daylight_duration,sunshine_duration,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant&timezone=Europe%2FBerlin&forecast_days=3&models=dmi_seamless
